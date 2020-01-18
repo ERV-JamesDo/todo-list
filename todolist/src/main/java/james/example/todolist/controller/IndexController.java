7a89004f71c543f3c61b5625566ccb5a6b827a4a
James-Do <hoang.mbios@gmail.com>
2020-01-18 16:39:35 +0700

    Add index controller

---
+++ todolist/src/main/java/james/example/todolist/controller/IndexController.java
@@ -0,0 +1,16 @@
+package james.example.todolist.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello and Welcome to the TODO application. You can create a new Work by making a POST request to /api/works endpoint.";
+    }
+}
